8368e18bb29d95e098173dbc951f8f8e11d56d2c
Erik <erik.engcomp@gmail.com>
2013-12-10 19:53:35 -0200

    criando_projeto

---
+++ Sistema_Escolar/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SistemaEscolar::Application.config.secret_key_base = '3b90b6284f97de343a4a87856a118e96593498b25c1cab99ae987414901f7eac86a4b36520850c337a08e2582b98127a209649ea063727fa93ef08a785618cdd'
